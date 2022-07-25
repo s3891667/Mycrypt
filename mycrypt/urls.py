ea7a5f2887eff8b479bb015267d66a6e238ac6a3
s3891667 <s3891667@rmit.edu.vn>
2022-07-25 15:37:46 +1000

    add fucntions

--- mycrypt/urls.py
+++ mycrypt/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'mycrypt'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.logIn, name='login'),
+    path('logout/', views.logOut, name='logout'),
+    path('signup/', views.signUp, name='signup')
+
 ]
--- mycrypt/views.py
+++ mycrypt/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate,login
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.models import User
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return redirect('mycrypt/index.html')
+
+def home(request):
+    if not request.user.is_authenticated:
+        return render(request, 'mycrypt/index.html')
+    else:
+        return redirect('mycrypt/home.html')
+
 
 def logIn(request):
     userName = request.POST['username']
     pw = request.POST['password']
-    user = authenticate(request,userName = userName,pw = pw)
-    
-    if user is not None : 
-        login(request,user)
-        
-# def logout(request):
-# def signUp(request):
+    user = authenticate(request, userName=userName, pw=pw)
 
+    if user is not None:
+        login(request, user)
+        return redirect('mycrypt/home.html')
+    else:
+        return HttpResponse("Please check your account again! ")
+
+
+def logOut(request):
+    logout(request)
+    return redirect('mycrypt/index.html')
+
+
+def signUp(request):
+    print("helloworld")
