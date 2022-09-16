1126222e0f91568e0c2941f65269335b12ae3e32
s3891667 <s3891667@rmit.edu.vn>
2022-09-16 13:44:31 +1000

    add

--- mycrypt/admin.py
+++ mycrypt/admin.py
@@ -7,26 +7,18 @@ from django.contrib.auth import get_permission_codename
 admin.site.register(User)
 
 
-@admin.action(description='Published')
-@admin.action(permissions=['upload'])
+@admin.action(description='Set marked article to published')
 class addArticle(admin.ModelAdmin):
-    def upload(self, modeladmin, request, queryset):
+    def upload(self, request, queryset):
         status = queryset.update(status='p')
         self.message_user(request, ngettext(
             '%d article was successfully marked as published.',
             '%d articles were successfully marked as published.',
             status
         ) % status, messages.SUCCESS)
-
-    list_article = ['title', 'date']
+    list_display = ['title', 'date', 'status']
     ordering = ['date']
     actions = [upload]
-
-    def has_publish_permission(self, request):
-        """Does the user have the publish permission?"""
-        opts = self.opts
-        codename = get_permission_codename('upload', opts)
-        return request.user.has_perm('%s.%s' % (opts.app_label, codename))
 
 
 admin.site.register(Content, addArticle)
