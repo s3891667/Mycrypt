dc1fdd486c769f4e978479c54e76e393b69386b2
s3891667 <s3891667@rmit.edu.vn>
2022-09-23 02:44:03 +1000

    implement

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-def mys(A, l, u, k):
-    if(l == u):
-        if(A[l] == k):
-            return 1
-        else:
-            return 0
-    else:
-        m = int((l+u-1)/2)
-        return mys(A, l, m, k) + mys(A, m+1, u, k)
-A = [0,1,2,3,4,5,6,7,8,9]
-print(mys(A,0,9,8))
+# def mys(A, l, u, k):
+#     if(l == u):
+#         if(A[l] == k):
+#             return 1
+#         else:
+#             return 0
+#     else:
+#         m = int((l+u-1)/2)
+#         return mys(A, l, m, k) + mys(A, m+1, u, k)
+# A = [0,1,2,3,4,5,6,7,8,9]
+# print(mys(A,0,9,8))
