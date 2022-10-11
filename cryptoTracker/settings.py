46f96a080665a062f97676e61ea04c4b5ef6c8ea
s3891667 <s3891667@rmit.edu.vn>
2022-10-12 01:56:46 +1100

    merge

--- cryptoTracker/settings.py
+++ cryptoTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5^w251k(sift%_dx@q8=p7a)!s3o3a0hscw5+#(e-trl9w=gl)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'cryptoTracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
 }
 
 
